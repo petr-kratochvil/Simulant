ff908cb71d1c4f4872637551900fca0246d96125
Rypouš Sloní <srypous@gmail.com>
2016-02-05 15:07:56 +0100

    Game21Console statistics with -seq

--- Game21Console/Main.cpp
+++ Game21Console/Main.cpp
@@ -54,6 +54,7 @@ int main(int argc, char** argv)
 		{
 			simulator.spinOneStart();
 			const Spin& spin = simulator.getLastSpin();
+			statistics.addSpin(spin);
 			int win = spin.getTotalWin();
 			totalCount++;
 			totalWin += win;
